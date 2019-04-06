343e170bb4ceedc5a43045ecc39741a1a3974b99
madt1m <pietrotirenna.pt@gmail.com>
2019-04-06 16:00:09 +0200

    First init w/ Travis

---
+++ src/main/java/it/polito/ai/pedibus/PedibusApplication.java
@@ -0,0 +1,13 @@
+package it.polito.ai.pedibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedibusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedibusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/polito/ai/pedibus/PedibusApplicationTests.java
@@ -0,0 +1,16 @@
+package it.polito.ai.pedibus;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PedibusApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
