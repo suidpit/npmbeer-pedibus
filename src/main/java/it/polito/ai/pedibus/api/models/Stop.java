c146fa32f51f79a6b4247b9399ed2e7505fcf5a6
madt1m <pietrotirenna.pt@gmail.com>
2019-04-13 12:28:14 +0200

    lab 2: Implemented basic structure

---
+++ src/main/java/it/polito/ai/pedibus/api/models/Stop.java
@@ -0,0 +1,17 @@
+package it.polito.ai.pedibus.api.models;
+
+import lombok.Data;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+
+@Data
+public class Stop {
+
+    @Id
+    private ObjectId _id;
+    private Integer line_id;
+    private String name;
+    private Integer time;
+    private String position;
+    private Boolean up_down;
+}
