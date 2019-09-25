c3de9f3461fee72b9d0c7f73eb503005aedcc512
madt1m <pietrotirenna.pt@gmail.com>
2019-09-26 00:21:44 +0200

    Events endpoints are now authenticated, per-account

--- src/main/java/it/polito/ai/pedibus/api/controllers/NotificationController.java
+++ src/main/java/it/polito/ai/pedibus/api/controllers/NotificationController.java
@@ -1,38 +1,40 @@
 package it.polito.ai.pedibus.api.controllers;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.codec.ServerSentEvent;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalTime;
 import java.util.Date;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 
 @CrossOrigin
 @RestController
 @RequestMapping("/events")
 public class NotificationController {
-    private List<Consumer<String>> listeners = new CopyOnWriteArrayList<>();
+    private Map<String, Consumer<String>> listeners = new ConcurrentHashMap<>();
 
-    public NotificationController() {
-
-    }
 
     @RequestMapping(value = "/write", method = RequestMethod.POST)
-    public String writeThing(@RequestBody String message) {
-        listeners.forEach(c -> c.accept(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " " + message));
+    public String writeThing(Authentication authentication, @RequestBody String message) {
+        String name = authentication.getName();
+        listeners.get(name).accept(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " ["+name+"] " + message);
         return "Done";
     }
 
     @GetMapping(value="/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
-    public Flux<String> streamEvents() {
+    public Flux<String> streamEvents(Authentication authentication) {
+        String name = authentication.getName();
         return Flux.create(sink -> {
-            listeners.add(sink::next);
+            listeners.put(name, sink::next);
         });
+    }
+
+    @GetMapping(value="/lol")
+    public String getUser(Authentication authentication) {
+        return authentication.getName();
     }
 }
